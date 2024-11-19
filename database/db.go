497978569ab8c9e7e0d524aa10b5c074f303bed0
Cabra Viva <greencoder001@outlook.com>
2024-11-19 19:04:49 +0100

    Write log file, save db in ~/Dampfer, log when dev env

--- database/db.go
+++ database/db.go
@@ -3,28 +3,56 @@ package database
 import (
 	"Dampfer/utils"
 	"database/sql"
+	"os"
+	"path/filepath"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
-// Initialize the database connection and create the users table if it doesn't exist
+// Initialize the database connection and create the necessary tables
 func init() {
 	var err error
 
-	DB, err = sql.Open("sqlite3", "./Dampfer.db")
+	// Get the home directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		utils.Log.Panic("Error retrieving home directory: ", err)
+		panic(err)
+	}
+
+	// Define the Dampfer folder and Dampfer.db file path
+	dbDir := filepath.Join(homeDir, "Dampfer")
+	dbFilePath := filepath.Join(dbDir, "Dampfer.db")
+
+	// Create the directory if it doesn't exist
+	err = os.MkdirAll(dbDir, 0755)
+	if err != nil {
+		utils.Log.Panic("Error creating database directory: ", err)
+		panic(err)
+	}
+
+	// Open the SQLite database
+	DB, err = sql.Open("sqlite3", dbFilePath)
 	if err != nil {
 		utils.Log.Panic("Failed to open database: ", err)
 		panic(err)
 	}
 
-	// Create the users table with permissions
-	_, err = DB.Exec(`CREATE TABLE IF NOT EXISTS users (
+	// Create the necessary tables
+	createUserTable()
+}
+
+// createUserTable creates the users table if it doesn't already exist
+func createUserTable() {
+	// Create the users table with permission field
+	query := `CREATE TABLE IF NOT EXISTS users (
         username TEXT PRIMARY KEY,
         password_hash TEXT NOT NULL,
         permission TEXT NOT NULL CHECK (permission IN ('system-admin', 'admin', 'insight'))
-    )`)
+    )`
+	_, err := DB.Exec(query)
 	if err != nil {
 		utils.Log.Panic("Failed to create users table: ", err)
 		panic(err)
--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"net/http"
-
 	"Dampfer/auth"
-	"Dampfer/docker"
+	"Dampfer/docker" // Import the restartCrash package
 	"Dampfer/utils"
 	"Dampfer/web"
 
-	_ "Dampfer/database" // Import to init db
+	_ "Dampfer/database" // Necessary to init database
+	"net/http"
 )
 
 var svelteFS http.Handler // Shared across dev.go and prod.go
 
+var isDEV bool // Set by dev.go or prod.go
+
 func main() {
 	// Logging
 	utils.InitLogger()
+
+	if isDEV {
+		utils.Log.Warn("Using DEVELOPMENT mode. All files will be served from the local FS and aren't embedded.")
+	} else {
+		utils.Log.Info("Starting in PRODUCTION mode. Webserver will be using embedded files. That's a good sign 👍")
+	}
 
 	// Check if Docker is installed and log result
 	docker.DoStartupInstallChecks()
@@ -32,7 +39,3 @@ func main() {
 	// Start Server
 	go web.StartServer()
 }
-
-// TODO: Protect API by user auth
-// TODO: Implement Login in frontend
-// TODO: User Creation UI
