6c88cefce108bdefdd60107092259bf55ad37a41
Cabra Viva <greencoder001@outlook.com>
2025-02-09 15:35:38 +0100

    Tags are properly sorted

--- svelte/src/pages/images/ImageBox.svelte
+++ svelte/src/pages/images/ImageBox.svelte
@@ -8,7 +8,7 @@
   import { getAverageBackgroundColor, searchIcons } from "../../script/icongen";
   import knorry from "knorry";
   import { getCredentials } from "../../script/login";
-  import { Button, Dropdown, Radio } from "flowbite-svelte";
+  import { Button, Dropdown, DropdownDivider, Radio } from "flowbite-svelte";
   import { ChevronDownOutline } from "flowbite-svelte-icons";
 
   let { pushAlert, updatePage, imagesWithSameTag } = $props() as {
@@ -39,8 +39,30 @@
   });
 
   // TODO: sort tags so that latest is on top, 2. is nightly and then all others are sorted by default array.sort
+  const sortedTags: (string | null)[] = [null, null];
+  const preSortedTags = imagesWithSameTag.tags.sort();
 
-  let selectedTag = $state(imagesWithSameTag.tags[0]);
+  for (const tagEl of preSortedTags) {
+    if (tagEl === "latest") {
+      sortedTags[0] = "latest";
+    } else if (tagEl === "nightly") {
+      sortedTags[1] = "nightly";
+    } else {
+      sortedTags.push(tagEl);
+    }
+  }
+
+  let firstTagNonNull: string;
+
+  if (sortedTags[0] !== null) {
+    firstTagNonNull = sortedTags[0];
+  } else if (sortedTags[1] !== null) {
+    firstTagNonNull = sortedTags[1];
+  } else {
+    firstTagNonNull = `${sortedTags[2]}`;
+  }
+
+  let selectedTag = $state(firstTagNonNull);
 </script>
 
 <!-- TODO:
@@ -68,15 +90,29 @@
     <span class="tags">
       <Button>
         :{selectedTag}
-        {#if imagesWithSameTag.tags.length > 1}
+        {#if sortedTags.filter((t) => t !== null).length > 1}
           <ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" />
         {/if}
       </Button>
-      {#if imagesWithSameTag.tags.length > 1}
+      {#if sortedTags.filter((t) => t !== null).length > 1}
         <Dropdown class="w-48 p-3 space-y-1">
-          {#each imagesWithSameTag.tags as tag}
+          {#if sortedTags[0] !== null || sortedTags[1] !== null}
+            {#each sortedTags.slice(0, 2) as tag}
+              {#if tag !== null}
+                <li
+                  class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600"
+                >
+                  <Radio name="selectedTag" bind:group={selectedTag} value={tag}
+                    >{tag}</Radio
+                  >
+                </li>
+              {/if}
+            {/each}
+            <DropdownDivider />
+          {/if}
+          {#each sortedTags.slice(2) as tag}
             <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-              <Radio name="group2" bind:group={selectedTag} value={tag}
+              <Radio name="selectedTag" bind:group={selectedTag} value={tag}
                 >{tag}</Radio
               >
             </li>
